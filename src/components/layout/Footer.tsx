79f579a4ccc411d5eec093cc052cb0bcd9f00d4e
Min <min@3drens.com>
2023-08-19 23:22:49 +0800

    feat: add Footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,24 @@
+import { HStack, Image, Text, VStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import TMDBLogo from '../../assets/TMDB-logo.png'
+
+const thisYear = new Date().getFullYear()
+
+export function Footer () {
+  return (
+    <VStack p='8' justifyContent='center' gap={6}>
+      <HStack gap='4'>
+        <Text>© 2018 - {thisYear}</Text>
+        <Text fontSize='md' fontWeight='600'>
+          <Link to='https://minhong.me'>minhong.me</Link>
+        </Text>
+      </HStack>
+      <HStack gap='4' justifyContent='center'>
+        <Text>Powered by</Text>
+        <Link to="https://www.themoviedb.org/">
+          <Image src={TMDBLogo}/>
+        </Link>
+      </HStack>
+    </VStack>
+  )
+}
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,15 +1,15 @@
 import { Box } from '@chakra-ui/react'
 import { Header } from './Header'
 import { Outlet } from 'react-router-dom'
+import { Footer } from './Footer'
 
 export function Layout () {
   return (
     <Box transition="0.5s ease-out" minHeight="100vh">
-      <Box margin="0 auto" maxWidth={1000}>
+      <Box margin="0 auto" maxWidth={1200}>
         <Header />
-        <Box as="main" marginY={22}>
-          <Outlet />
-        </Box>
+        <Box as="main" marginY={16}><Outlet /></Box>
+        <Footer/>
       </Box>
     </Box>
   )
