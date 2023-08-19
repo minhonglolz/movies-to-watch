67777c88331708bd0b63e59ac99b7a142079c7cf
Min <min@3drens.com>
2023-08-19 18:26:36 +0800

    chore: init @eslint/config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,51 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   extends: [
+    'standard-with-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   overrides: [
     {
-      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
-      extends: 'standard-with-typescript',
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+        '*.ts',
+        '*.tsx'
+      ],
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: ['./tsconfig.json', './tsconfig.node.json'],
-        tsconfigRootDir: __dirname,
-      },
-      rules: {
-        'react-refresh/only-export-components': [ 'warn', { allowConstantExport: true }],
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/triple-slash-reference': 'off',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname
+
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-indent': ['warn', 2, {
+      checkAttributes: true,
+      indentLogicalExpressions: true
+    }],
+    'react/jsx-curly-spacing': ['warn', { when: 'never', children: true }]
+  }
 }
