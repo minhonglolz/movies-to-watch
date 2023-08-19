6d1cdaac6a3a316b09816c4cfdeb520da8f53d29
Min <min@3drens.com>
2023-08-19 14:47:05 +0800

    fix: main.tsx eslint

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const target = document.getElementById('root')
+if (target == null) throw new Error()
+
+ReactDOM.createRoot(target).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
