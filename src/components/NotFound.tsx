e476c072000cd4e7f51af48ed1b9aa8ad7d0a3a8
Min <min@3drens.com>
2023-08-19 22:57:44 +0800

    feat: add 404 route

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,19 @@
+import { Box, Button, Heading, Image } from '@chakra-ui/react'
+import ErrorImage from '../assets/404.png'
+import { Link } from 'react-router-dom'
+
+export function NotFound () {
+  return (
+    <Box>
+      <Image w='50%' m='16px auto' src={ErrorImage} alt="404 error" />
+      <Box textAlign='center'>
+        <Heading>找不到網頁</Heading>
+        <Button mt={4}>
+          <Link to="/">
+            回首頁
+          </Link>
+        </Button>
+      </Box>
+    </Box>
+  )
+}
--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,25 +1,21 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { WatchList } from './WatchList/WatchList'
 import App from '../App'
+import { NotFound } from './NotFound'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App/>,
     children: [
-      {
-        path: 'watch-list',
-        element: <WatchList/>
-      }
+      { path: 'watch-list', element: <WatchList/> },
+      { path: '*', element: <NotFound /> }
     ]
   }
 ])
 
 export function Routes () {
   return (
-    <RouterProvider
-          router={router}
-          // fallbackElement={<BigSpinner />}
-        />
+    <RouterProvider router={router} />
   )
 }
