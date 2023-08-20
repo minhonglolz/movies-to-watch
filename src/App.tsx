32ad0b5c10f14cab538c3f510473c89ddff4e89c
Min <min@3drens.com>
2023-08-20 11:18:03 +0800

    chore: rename layout to Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from './styles/theme/theme'
-import { Layout } from './styles/tempLayout/Layout'
+import { Layout } from './components/Layout/Layout'
 
 function App () {
   return (
