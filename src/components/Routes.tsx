af11c60e0d843fc12f3947ec879f13596bfbc251
Min <min@3drens.com>
2023-08-20 16:33:31 +0800

    feat: add Home route

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -2,12 +2,14 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { WatchList } from './WatchList/WatchList'
 import App from '../App'
 import { NotFound } from './NotFound'
+import { Home } from './Home/Home'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App/>,
     children: [
+      { path: '', element: <Home/> },
       { path: 'watch-list', element: <WatchList/> },
       { path: '*', element: <NotFound /> }
     ]
