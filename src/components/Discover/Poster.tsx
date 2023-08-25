42a188d68adc2eb6ac802a22c7d525a34a9c7e53
Min <min@3drens.com>
2023-08-26 01:01:54 +0800

    style: add Poster transition

--- src/components/Discover/Poster.tsx
+++ src/components/Discover/Poster.tsx
@@ -1,6 +1,7 @@
 import { Image, useMediaQuery } from '@chakra-ui/react'
 import { POSTER_IMAGE_URL_X2, POSTER_IMAGE_URL_X1 } from '../../constants/movies'
 import noImage from '../../assets/noImage.svg'
+import { useState } from 'react'
 
 interface Props {
   posterUrl?: string
@@ -8,19 +9,24 @@ interface Props {
 
 export function Poster ({ posterUrl }: Props) {
   const [isLargerThanLg] = useMediaQuery('(min-width: 960px)')
+  const [isLoad, setIsLoad] = useState(false)
   return (
     <>
       {posterUrl != null
         ? <Image
+            opacity={isLoad ? 1 : 0}
+            backgroundColor={'#000'}
+            transition={'all .5s'}
+            onLoad={() => setIsLoad(true)}
             borderTopRadius={12}
             src={`${isLargerThanLg ? POSTER_IMAGE_URL_X2 : POSTER_IMAGE_URL_X1}${posterUrl}`}
-              />
+            />
         : <Image
             h={'100%'}
             objectFit={'cover'}
             borderTopRadius={12}
             src={noImage}
-              />
+            />
           }
     </>
   )
