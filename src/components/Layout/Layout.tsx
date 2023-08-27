38fb314a92a0fe1be2b8c75da983a79cdffd3e77
Min <min@3drens.com>
2023-08-27 19:35:00 +0800

    style: Layout marginY

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ export function Layout () {
     <Box transition="0.5s ease-out" minHeight="100vh">
       <Box margin="0 auto" maxWidth={1200}>
         <TheHeader />
-        <Box as="main" marginY={8} p={4}><Outlet /></Box>
+        <Box as="main" marginY={6} p={4}><Outlet /></Box>
         <TheFooter />
       </Box>
     </Box>
