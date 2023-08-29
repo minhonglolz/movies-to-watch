2d0236eb6f551491df325e2f1cac6bc797f88216
Min <min@3drens.com>
2023-08-30 00:19:56 +0800

    feat: axios Authorization in top

--- src/utils/tmdbBaseAxios.ts
+++ src/utils/tmdbBaseAxios.ts
@@ -6,6 +6,7 @@ export default function tmdbBaseAxios () {
   const axiosInstance = axios.create({
     baseURL: import.meta.env.TMDB_API_ROOT,
     headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
       'Content-Type': 'application/json',
       Accept: 'application/json'
     }
