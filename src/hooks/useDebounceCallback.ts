e4efd4029a5dfd834433c05b5144177375d078c0
Min <min@3drens.com>
2023-08-30 00:20:37 +0800

    feat: add useDebounceCallbck hook

---
+++ src/hooks/useDebounceCallback.ts
@@ -0,0 +1,13 @@
+import { useRef } from 'react'
+
+export default function useDebounceCallback () {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+  const debounceCallback = (callback: () => void, debounce = 1000) => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current)
+    timeoutRef.current = setTimeout(callback, debounce)
+  }
+  const clearRef = () => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current)
+  }
+  return { debounceCallback, clearRef }
+}
