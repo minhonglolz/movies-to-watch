b154f53c2b52342ca480b48380970c8b5a9f46de
Min <min@3drens.com>
2023-09-01 01:21:36 +0800

    feat: create useToast

---
+++ src/hooks/useToast.ts
@@ -0,0 +1,28 @@
+import { useToast as useBaseToast } from '@chakra-ui/react'
+
+const toastProps = {
+  position: 'top-right',
+  duration: 1000,
+  isClosable: true
+} as const
+
+export function useToast () {
+  const toast = useBaseToast()
+  const showSuccessToast = (title: string) => {
+    toast({
+      title,
+      status: 'success',
+      ...toastProps
+    })
+  }
+
+  const showErrorToast = (title: string) => {
+    toast({
+      title,
+      status: 'error',
+      ...toastProps
+    })
+  }
+
+  return { showSuccessToast, showErrorToast }
+}
