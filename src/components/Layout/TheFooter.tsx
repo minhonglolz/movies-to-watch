cd5b936812347eb290242f621315b8bcd0553509
Min <min@3drens.com>
2023-09-02 18:15:10 +0800

    fix: TheFooter since

--- src/components/Layout/TheFooter.tsx
+++ src/components/Layout/TheFooter.tsx
@@ -2,13 +2,11 @@ import { Flex, HStack, Image, Text } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import TMDBLogo from '../../assets/TMDB-logo.png'
 
-const thisYear = new Date().getFullYear()
-
 export function TheFooter () {
   return (
     <HStack p='4' justifyContent='space-between' gap={6} flexDirection={['column-reverse', 'row']}>
       <HStack gap='4'>
-        <Text>© 2018 - {thisYear}</Text>
+        <Text>© 2023</Text>
         <Text fontSize='md' fontWeight='600'>
           <Link target='_blank' to='https://minhong.me'>minhong.me</Link>
         </Text>
