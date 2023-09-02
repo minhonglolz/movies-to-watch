d998257e11fd2ec5ad17b3f91554c9c85185acc1
Min <z574512z@gmail.com>
2023-09-03 01:38:51 +0800

    refactor: remove unused

--- src/hooks/useTmdbSWR.ts
+++ src/hooks/useTmdbSWR.ts
@@ -3,5 +3,5 @@ import { tmdbSWRFetcher } from '../utils/swrFetcher'
 import { type TmdbErrorResponse } from '../types/Tmdb/TmdbErrorResponse'
 
 export function useTmdbSWR<Response> (key: Key) {
-  return useSWR<Response, TmdbErrorResponse>(key, tmdbSWRFetcher, { focusThrottleInterval: 500000 })
+  return useSWR<Response, TmdbErrorResponse>(key, tmdbSWRFetcher)
 }
