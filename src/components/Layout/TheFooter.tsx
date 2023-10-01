cfa94d1e975a9a258b2c9f9ea83c3fa977e3dd8f
黃閔弘 <minhong@husky.local>
2023-10-01 11:34:57 +0800

    feat: add github icon at footer

--- src/components/Layout/TheFooter.tsx
+++ src/components/Layout/TheFooter.tsx
@@ -1,13 +1,17 @@
 import { Flex, HStack, Image, Text } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import tmdbLogo from '../../assets/tmdbLogo.png'
+import { AiFillGithub } from 'react-icons/ai'
 
 export function TheFooter () {
   return (
-    <HStack as="footer" p='4' justifyContent='space-between' gap={6} flexDirection={['column-reverse', 'row']}>
+    <HStack fontSize="lg" as="footer" p='4' justifyContent='space-between' gap={6} flexDirection={['column-reverse', 'row']}>
       <HStack gap='4'>
         <Text>© 2023</Text>
-        <Text fontSize='md' fontWeight='600'>
+        <Link target='_blank' to="https://github.com/minhonglolz">
+          <AiFillGithub />
+        </Link>
+        <Text fontWeight='600'>
           <Link target='_blank' to='https://minhong.me'>minhong.me</Link>
         </Text>
       </HStack>
